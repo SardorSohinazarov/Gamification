9a552d26c56bd047632f9f4346f0657241006428
SardorSohinazarov <sardorstudent0618@gmail.com>
2025-07-02 23:02:04 +0500

    backend api

--- src/Gamification.Student.UI/Program.cs
+++ src/Gamification.Student.UI/Program.cs
@@ -9,9 +9,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient<ITestService, TestService>(
-    client => client.BaseAddress = new Uri("https://localhost:7114"));
+    client => client.BaseAddress = new Uri("https://gamification-production-b1e5.up.railway.app"));
 builder.Services.AddHttpClient<IQuizService, QuizService>(
-    client => client.BaseAddress = new Uri("https://localhost:7114"));
+    client => client.BaseAddress = new Uri("https://gamification-production-b1e5.up.railway.app"));
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
