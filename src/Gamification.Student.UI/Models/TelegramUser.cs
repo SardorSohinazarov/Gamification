d3adf1fbf6f2835b53a1b65974d5b688dc43b17a
SardorSohinazarov <sardorstudent0618@gmail.com>
2025-07-03 11:16:46 +0500

    User Profile

---
+++ src/Gamification.Student.UI/Models/TelegramUser.cs
@@ -0,0 +1,16 @@
+﻿namespace Gamification.Student.UI.Models
+{
+    public class TelegramUser
+    {
+        public long Id { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Username { get; set; }
+        public string Photo {
+            get
+            {
+                return $"https://t.me/i/userpic/320/{Username}.jpg";
+            } }
+        public string Language_code { get; set; }
+    }
+}
