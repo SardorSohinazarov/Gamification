8148f5d78486c240bff77f0e0b1f46fc1f1cc2f0
SardorSohinazarov <sardorstudent0618@gmail.com>
2025-07-04 22:37:05 +0500

    Soft Delete

--- src/Gamification.Student.UI/Services/Telegram/IUserService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Gamification.Student.UI.Models;
-
-namespace Gamification.Student.UI.Services.Telegram
-{
-    public interface IUserService
-    {
-        Task<TelegramUser> GetUserAsync();
-    }
-}
