a7652e31d9eb06e7031bc1d8001d3bb67daadf1b
SardorSohinazarov <sardorstudent0618@gmail.com>
2025-07-05 22:28:41 +0500

    Auth Logic

---
+++ src/Gamification.Application/Services/Accounts/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using Gamification.Application.DataTransferObjects.Auth;
+using Gamification.Application.Models;
+
+namespace Gamification.Application.Services.Accounts
+{
+    public interface IAccountService
+    {
+        Task<TokenDto> LoginAsync(LoginDto loginDto);
+        Task<TokenDto> LoginAsync(TelegramAuthData telegramAuthData);
+    }
+}
