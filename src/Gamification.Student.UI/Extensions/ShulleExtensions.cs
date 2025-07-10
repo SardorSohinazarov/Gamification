de1356a9e286588ee36a0f695dc1087d783009c9
SardorSohinazarov <sardorstudent0618@gmail.com>
2025-07-10 10:22:19 +0500

    Savollar va Javoblar ketma ketligi random bo'lishi uchun

---
+++ src/Gamification.Student.UI/Extensions/ShulleExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Gamification.Student.UI.Extensions
+{
+    public static class ShulleExtensions
+    {
+        public static T[] Shuffle<T>(this IEnumerable<T> list)
+        {
+            return list.OrderBy(x => Guid.NewGuid()).ToArray();
+        }
+    }
+}
